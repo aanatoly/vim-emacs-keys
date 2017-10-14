ba276184a58b9700bf7601a2e4c45dc229212978
Anatoly <aanatoly@gmail.com>
2017-10-14 18:38:34 +0300

    change word case

--- plugin/emacs-keys.vim
+++ plugin/emacs-keys.vim
@@ -23,10 +23,19 @@ autocmd CursorMovedI * let g:InsCol = col('.') |
 let s:emacs_py = resolve(expand('<sfile>:p:h')) ."/main.py"
 execute 'pyfile ' . s:emacs_py
 
-function! KillWord(arg)
+function! KillWord(mode)
     py kill_word()
 endfunc
-
 nnoremap <A-d> :call KillWord("n")<CR>
 inoremap <A-d> <C-o>:call KillWord("i")<CR>
+
+function! ChangeWordCase(mode, conv)
+    py change_word_case()
+endfunc
+nnoremap <A-u> :call ChangeWordCase("n", "u")<CR>
+inoremap <A-u> <C-o>:call ChangeWordCase("i", "u")<CR>
+nnoremap <A-l> :call ChangeWordCase("n", "l")<CR>
+inoremap <A-l> <C-o>:call ChangeWordCase("i", "l")<CR>
+nnoremap <A-c> :call ChangeWordCase("n", "c")<CR>
+inoremap <A-c> <C-o>:call ChangeWordCase("i", "c")<CR>
 
--- plugin/main.py
+++ plugin/main.py
@@ -1,6 +1,6 @@
 
 import vim
-
+import string
 
 def skip_chars(line, slen, pos, test):
     while pos < slen:
@@ -9,25 +9,35 @@ def skip_chars(line, slen, pos, test):
         pos += 1
     return pos
 
+def set_cursor(row, col):
+    vim.current.window.cursor = row + 1, col
+
+def get_cursor(mode='n'):
+    if mode == 'n':
+        row, col = vim.current.window.cursor
+        row -= 1
+    elif mode == 'i':
+        col = int(vim.eval("g:InsCol")) - 1
+        row = int(vim.eval("g:InsRow")) - 1
+    else:
+        raise Exception("Unknown mode " + mode)
+    return row, col
+
+def get_text():
+    return vim.current.line, len(vim.current.line)
+
+is_not_alnum = lambda x: not str.isalnum(x)
+is_alnum = lambda x: str.isalnum(x)
+is_upper = lambda x: str.isupper(x)
+is_lower_or_digit = lambda x: str.isdigit(x) or str.islower(x)
+
 ################################################
 # {{{ kill word
-def kill_alg_1(line, slen, pos):
-    if line[pos:pos+2].isspace():
-        pos = skip_chars(line, slen, pos, lambda x: str.isspace(x))
-    else:
-        if line[pos].isspace():
-            pos += 1
-        if line[pos].isalnum():
-            pos = skip_chars(line, slen, pos, lambda x: str.isalnum(x))
-        else:
-            pos = skip_chars(line, slen, pos, lambda x: str.isspace(x) or not str.isalnum(x))
-    return pos
 
 
 def kill_alg_2(line, slen, pos):
     # find word
-    test = lambda x: not str.isalnum(x)
-    anpos = skip_chars(line, slen, pos, test)
+    anpos = skip_chars(line, slen, pos, is_not_alnum)
 
     # if no word - delete rest of the line
     if anpos == slen:
@@ -38,55 +48,91 @@ def kill_alg_2(line, slen, pos):
     if '\t' in line[pos:anpos]:
         gap += 4
     if gap < 4:
-        test = lambda x: str.isupper(x)
-        anpos = skip_chars(line, slen, anpos, test)
-        test = lambda x: str.isdigit(x) or str.islower(x)
-        return skip_chars(line, slen, anpos, test)
+        anpos = skip_chars(line, slen, anpos, is_upper)
+        return skip_chars(line, slen, anpos, is_lower_or_digit)
 
     # otherwise delete the gap and bring word forward
     return anpos
 
 
 def kill_word():
-    mode = vim.eval("a:arg")
-    cw = vim.current.window
-    cb = vim.current.buffer
-    if mode == 'n':
-        row, col = cw.cursor
-        row -= 1
-    else:
-        col = int(vim.eval("g:InsCol")) - 1
-        row = int(vim.eval("g:InsRow")) - 1
-    line = cb[row]
-    slen = len(cb[row])
+    mode = vim.eval("a:mode")
+    buf = vim.current.buffer
+    line, slen = get_text()
+    row, col = get_cursor(mode)
 
     # delete empty lines
     if slen == 0:
-        while row < len(cb):
-            if len(cb[row]):
+        while row < len(buf):
+            if len(buf[row]):
                 break
-            del cb[row]
+            del buf[row]
         return
 
     # insert mode, end of line - join next line
     if col == slen:
         vim.command('/\w')
-        erow, ecol = cw.cursor
-        erow -= 1
+        erow, ecol = get_cursor()
         # if no word found - delete till the end of a buffer
         if erow < 1:
-            del cb[row + 1:]
-            cw.cursor = (row + 1, col)
+            del buf[row + 1:]
+            set_cursor(row, col)
             return
         # otherwise delete till the word
-        cb[row] += cb[erow][ecol:]
-        del cb[row + 1:erow + 1]
-        cw.cursor = (row + 1, col)
+        buf[row] += buf[erow][ecol:]
+        del buf[row + 1:erow + 1]
+        set_cursor(row, col)
     elif col == slen -1:
-        cb[row] = line[:col]
+        buf[row] = line[:col]
     else:
         pos = kill_alg_2(line, slen, col)
-        cb[row] = line[:col] + line[pos:]
-    slen = len(cb[row])
+        buf[row] = line[:col] + line[pos:]
+    slen = len(buf[row])
+# }}}
+
+################################################
+# {{{ chage word case
+
+conv = {
+    'l': string.lower,
+    'u': string.upper,
+    'c': string.capitalize
+}
+
+# FIXME: Can't make vim command "/\w' do right thing. So write it myself
+def find_word(mode, buf, row, col):
+    while row < len(buf):
+        line = buf[row]
+        slen = len(line)
+        while col < slen:
+            if line[col].isalnum():
+                return (row, col)
+            col += 1
+        col = 0
+        row += 1
+
+    return (None, None)
+
+
+def change_word_case():
+    mode = vim.eval("a:mode")
+    buf = vim.current.buffer
+
+    row, col = find_word(mode, buf, *get_cursor(mode))
+    if row is None:
+        return
+    line = buf[row]
+    slen = len(line)
+    ecol = skip_chars(line, slen, col, is_alnum)
+    func = conv[vim.eval("a:conv")]
+    buf[row] = line[:col] + func(line[col:ecol]) + line[ecol:]
+    set_cursor(row, ecol)
+# }}}
+
+################################################
+# {{{ cursor movements
+
+def find_word_end():
+    pass
 # }}}
 
